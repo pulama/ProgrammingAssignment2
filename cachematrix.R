6b00dee3756bf40fb26cf8f0b64c643648633969
pulama <lamapukar1@gmail.com>
2017-06-01 23:23:32 -0400

    edited R file cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,81 @@
-## Put comments here that give an overall description of what your
-## functions do
+# 'makematrix' does the following:
+## Creates four functions (set, get,setmatrix and getmatrix)
+## Creates two objects (x and m)
+## Creates list of names for all four functions
+makematrix<-function(x=matrix()){
+  m<-NULL
+  set<-function(y)
+  {x<<-y
+  m<<-NULL}
+  get<-function()x
+  setmatrix<-function(matrix)
+    m<<-matrix
+  getmatrix<-function()m
+  list(set=set,
+       get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
+}
 
-## Write a short comment describing this function
+# 'cacheSolve' does the following:
+## Retreives the getmatrix input from above object 'makematrix'
+## Checks if the result is set to NULL ,if not new matrix is introduced
+## Calculates and returns the inverse of matrix to parent environment
 
-makeCacheMatrix <- function(x = matrix()) {
-
+cacheSolve<-function(x,...){
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data.")
+    return(m)
+  }
+  data<-x$get()
+  m<-solve(data,...)
+  x$setmatrix(m)
+  m
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# Testing the object 'makematrix' and 'cacheSolve
+
+# Creating square matrix
+## sample<-matrix(c(2,3,4,1),nrow=2,ncol=2,byrow=TRUE)
+
+# Printing sample
+## sample
+##        [,1] [,2]
+## [1,]    2    3
+## [2,]    4    1
+
+#Printing inverse of sample required later while comparing cacheSolve.
+##solve(sample)
+##      [,1] [,2]
+##[1,] -0.1  0.3
+##[2,]  0.4 -0.2
+
+# Creating 'testmatrix'object taking 'makematrix' with an input 'sample'
+## testmatrix<-makematrix(sample)
+
+# Testing if 'testmatrix' includes all the functions within 'makematrix'
+##testmatrix$get()
+##       [,1] [,2]
+##[1,]    2    3
+##[2,]    4    1
+
+## testmatrix$getmatrix() (First run)
+## NULL
+
+# setting the sample matrix
+# testmatrix$set(sample)
+
+# cacheSolve(testmatrix) ( the results matches to solve(sample) above)
+##      [,1] [,2]
+## [1,] -0.1  0.3
+## [2,]  0.4 -0.2
+
+# testmatrix$getmatrix()( the results matches to solve(sample)above,Second run)
+##      [,1] [,2]
+## [1,] -0.1  0.3
+## [2,]  0.4 -0.2
+
+
